1ef181ed27a3ada6b7113eb5d601d0e213213c8f
guchen <email@example.com>
2019-07-05 22:25:42 +0800

    Initial commit

---
+++ AutoTestSite/urls.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+"""AutoTestSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from showcase import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
+    url(r'^404/', views.notfound),
+    url(r'^form_builder/', views.form_builder),
+    # 入口页面为127.0.0.1:8000/base,不是index
+    url(r'^basebase/', views.base),
+    url(r'^layouts/', views.layouts),
+    url(r'^showcase/', views.showcase),
+    url(r'^ytoAutoCaseCount/', views.ytoAutoCaseCount),
+    url(r'^API_MASTER/', views.API_MASTER),
+    url(r'^apitest/', views.apitest),
+    url(r'^addProject/',views.addProject),
+    url(r'^projectList/',views.projectList),
+    url(r'^bootstrapTable/',views.bootstrapTable),
+    url(r'^getdata/',views.getdata),
+]
---
+++ showcase/views.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+import pymysql
+import json
+import requests
+
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'showcase/index.html')
+
+
+def notfound(request):
+
+    return render(request, 'showcase/404.html')
+
+
+def form_builder(request):
+
+    return render(request, 'showcase/form_builder.html')
+
+
+def base(request):
+
+    return render(request, 'showcase/base.html')
+
+
+def layouts(request):
+
+    return render(request, 'showcase/layouts.html')
+
+
+def API_MASTER(request):
+
+    return render(request, 'showcase/API_MASTER.html')
+
+
+def showcase(request):
+    """
+    从数据库中读取作者的用例信息，并用于前台可视化展示
+    :param request:
+    :return:
+    """
+    # 注意修改此处mysql服务的ip
+    db = pymysql.connect("localhost", "root", "guchen", "guchen_test", charset='utf8')
+    cursor = db.cursor()
+    sql = "select * from userCaseInfo"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results
+    # 向js中传递数据必须json.dumps()处理
+    return render(request, "showcase/showcase.html", {'caseInfo': json.dumps(list(results))})
+
+
+def apitest(request):
+
+    request_method = request.GET['request_method']
+    api_url = request.GET['api_url']
+    print request_method
+    print api_url
+    r = requests.get(api_url)
+    print r.content
+    return render(request, 'showcase/API_MASTER.html',{'response': r.content})
+
+def ytoAutoCaseCount(request):
+    db = pymysql.connect("192.168.207.160", "root", "123qwe!@#", "autotest", charset='utf8')
+    cursor = db.cursor()
+    advertisementCount = "select total,succ,fail,percent from advertisement ORDER BY id DESC limit 1"
+    membercenterCount = "select total,succ,fail,percent from membercenter ORDER BY id DESC limit 1"
+    permissionCount = "select total,succ,fail,percent from permission ORDER BY id DESC LIMIT 1"
+    saasCount = "select total,succ,fail,percent from saas ORDER BY id DESC LIMIT 1"
+    cursor.execute(advertisementCount)
+    advertisement_results = cursor.fetchall()
+    cursor.execute(membercenterCount)
+    membercenter_results = cursor.fetchall()
+    cursor.execute(permissionCount)
+    permission_results = cursor.fetchall()
+    cursor.execute(saasCount)
+    saas_results = cursor.fetchall()
+
+    advertisement_results_list=list(advertisement_results[0])
+    advertisement_results_list.append('advertisement')
+    membercenter_results_list=list(membercenter_results[0])
+    membercenter_results_list.append('membercenter')
+    permission_results_list=list(permission_results[0])
+    permission_results_list.append('permission')
+    saas_results_list=list(saas_results[0])
+    saas_results_list.append('saas')
+
+
+    results = (advertisement_results_list,membercenter_results_list,permission_results_list,saas_results_list)
+    # print results
+
+    #获取用例总数
+    advertisementNumSQL= 'select count(*) from advertisement'
+    cursor.execute(advertisementNumSQL)
+    advertisementNum = cursor.fetchall()
+
+    membercenterNumSQL = 'select count(*) from membercenter'
+    cursor.execute(membercenterNumSQL)
+    membercenterNum = cursor.fetchall()
+
+    permissionNumSQL = 'select count(*) from permission'
+    cursor.execute(permissionNumSQL)
+    permissionNum = cursor.fetchall()
+
+    saasNumSQL = 'select count(*) from saas'
+    cursor.execute(saasNumSQL)
+    saasNum = cursor.fetchall()
+
+    totalCount= int(results[0][0])+int(results[1][0])+int(results[2][0])+int(results[3][0])
+    sucCount=int(results[0][1])+int(results[1][1])+int(results[2][1])+int(results[3][1])
+    passRate=sucCount/float(totalCount)
+    passRate ="%.2f%%" % (passRate * 100)
+    # print totalCount
+    # print results
+    # 向js中传递数据必须json.dumps()处理
+    return render(request, "showcase/ytoAutoCaseCount.html", {'caseInfo': json.dumps(list(results)),
+                                                              'totalCount':totalCount,
+                                                              'sucCount':sucCount,
+                                                              'passRate':passRate})
+
+# ytoAutoCaseCount()
+
+
+def addProject(request):
+    '''
+    新增自动化测试项目
+    :param request:
+    :return:
+    '''
+    # db = pymysql.connect("192.168.207.160", "root", "123qwe!@#", "autotest", charset='utf8')
+    db = pymysql.connect("localhost", "root", "guchen", "guchen_test", charset='utf8')
+    cursor = db.cursor()
+    if request.method == "POST":
+        projectName = request.POST.get("tableName", None)
+        comment = request.POST.get("tableComment", None)
+        print projectName, comment
+        addProjectSQL = (
+            "CREATE TABLE `%s` ("
+            "`id` int(32) NOT NULL AUTO_INCREMENT,"
+            " `total` varchar(64) DEFAULT NULL,"
+            "`succ` varchar(64) DEFAULT NULL,"
+            " `fail` varchar(64) DEFAULT NULL,"
+            "`percent` varchar(64) DEFAULT NULL,"
+            "`author` varchar(64) DEFAULT NULL,"
+            " `add_time` datetime DEFAULT CURRENT_TIMESTAMP,"
+            " PRIMARY KEY (`id`))"
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='%s'"
+            % (projectName, comment)
+        )
+        cursor.execute(addProjectSQL)
+        cursor.close()
+    status = 'success'
+    return render(request,"showcase/projectList.html",{'status':json.dumps(status)})
+
+def projectList(requset):
+
+    return render(requset,"showcase/projectList.html")
+
+
+def bootstrapTable(request):
+    return render(request, 'showcase/bootstrapTable.html')
+
+
+def getdata(request):
+
+    # 接收url传递来的search_kw参数值
+    search_kw = request.GET.get('search_kw')
+    print 'search_kw的值为：%s' % search_kw
+
+    # db = pymysql.connect("192.168.207.160", "root", "123qwe!@#", "autotest", charset='utf8')
+    db = pymysql.connect("localhost", "root", "guchen", "autotest", charset='utf8')
+
+    cursor = db.cursor()
+    rows = []
+    # 根据是否存在搜索关键字执行不同sql,用来返回符合条件的数据
+    if search_kw:
+        # saasCount = "select id,total,succ,fail,percent from saas where id like '%%%s%%' or total like '%%%s%%'" % (search_kw,search_kw)
+        saasCount = "select project_name,project_code,description,project_manager from project " \
+                    "where project_name like '%%%s%%' or description like '%%%s%%' " \
+                    "or project_manager like '%%%s%%'" % (search_kw, search_kw, search_kw)
+    else:
+        saasCount = "select project_name,project_code,description,project_manager from project"
+    cursor.execute(saasCount)
+    saas_results = cursor.fetchall()
+    print list(saas_results)
+    for i in list(saas_results):
+        print i
+        # 将数组中的每个元素提取出来拼接为rows的内容
+        rows.append({"project_name": i[0], "project_code": i[1], "description": i[2], "project_manager": i[3]})
+    print rows
+    # rows返回为json数组
+    return HttpResponse(json.dumps(rows))
